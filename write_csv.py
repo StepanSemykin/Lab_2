f63dd58a502b49076fdd2dd28c273a2d91e69c0b
StepanSemykin <semykin.2003@mail.ru>
2022-10-24 23:02:02 +0400

    измения в docstring

--- write_csv.py
+++ write_csv.py
@@ -2,12 +2,11 @@ import csv
 import os
 
 def write_csv(directory_obj : str, start : str, name: str):
-    """Writes the absolute and relative path of the image to csv.
-
+    """Writes the absolute and relative path of the image to csv, return NONE.
     Args:
-        directory_obj (str): full path to the file
-        start (str): intermediate file path
-        name (str): name of the image class
+        directory_obj (str): full path to the folder.
+        start (str): intermediate folder path.
+        name (str): object class.
     """
     file = "patch.csv"
     f = open(file, "a", encoding = "utf-8", newline = "")
@@ -20,7 +19,7 @@ def write_csv(directory_obj : str, start : str, name: str):
         f_writer.writerow({"Absolut_path": directory_obj + "\\" + i, "Relative_patch":  r_directory_obj + "\\" + i, "Class": name})    
     
 def main():
-    """Separates code blocks"""
+    """Separates code blocks."""
     directory_rose = "D:\VS Code project\Images.py\dataset\ rose"
     directory_tulip = "D:\VS Code project\Images.py\dataset\ tulip"
     start_rose = "D:\VS Code project\Images.py\\"
