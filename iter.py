ce2033c557a9acc89db389189d9e91e6c29575ce
StepanSemykin <semykin.2003@mail.ru>
2022-10-26 02:00:22 +0400

    Добавлены docstring

--- iter.py
+++ iter.py
@@ -5,7 +5,7 @@ class Iterator:
         """Сonstructor of the class object, return NONE.
         Args:
             directory (str): full path to the folder.
-            name (str): object classs.
+            name (str): object class.
         """
         self.directory = directory
         self.name = name
@@ -20,9 +20,19 @@ class Iterator:
                     self.read_list.append(i["Absolut_path"])   
 
     def __iter__(self):
+        """Return iterator object.
+        Returns:
+            self: iterstor object.
+        """
         return self
     
     def __next__(self):
+        """Return the next element in the sequence.
+        Raises:
+            StopIteration: stopping the iterator.
+        Returns:
+           str: patch to the file.
+        """
         if self.count < len(self.read_list):
             self.count += 1
             return self.read_list[self.count]
@@ -31,6 +41,7 @@ class Iterator:
             raise StopIteration
        
 def main():
+    """Separates code blocks."""
     s = Iterator("D:\Lab Python\Lab_2\copy_patch.csv", "rose")     
 
     print(next(s))
