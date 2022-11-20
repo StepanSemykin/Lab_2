c679a4fdcfb06beb62ff57d5633a68ecc101adf0
StepanSemykin <semykin.2003@mail.ru>
2022-11-20 18:17:46 +0400

    Создание окна с кнопками

--- write_csv.py
+++ write_csv.py
@@ -1,14 +1,14 @@
 import csv
 import os
 
-def write_csv(directory_obj : str, start : str, name: str):
+def write_csv(directory_obj : str, start : str,  file: str, name: str):
     """Writes the absolute and relative path of the image to csv, return NONE.
     Args:
         directory_obj (str): full path to the folder.
         start (str): intermediate folder path.
         name (str): object class.
     """
-    file = "patch.csv"
+    file = f"{file} annotation.csv"
     f = open(file, "a", encoding = "utf-8", newline = "")
     f_writer = csv.DictWriter(f, fieldnames = ["Absolut_path", "Relative_patch", "Class"], delimiter = "|")
     
@@ -20,13 +20,14 @@ def write_csv(directory_obj : str, start : str, name: str):
     
 def main():
     """Separates code blocks."""
-    directory_rose = "D:\VS Code project\Images.py\dataset\ rose"
-    directory_tulip = "D:\VS Code project\Images.py\dataset\ tulip"
-    start_rose = "D:\VS Code project\Images.py\\"
-    start_tulip = "D:\VS Code project\Images.py\\"
+    directory_rose = "D:\Lab Python\Lab_1\dataset\ rose"
+    directory_tulip = "D:\Lab Python\Lab_1\dataset\ tulip"
+    start_rose = "D:\Lab Python\Lab_1\\"
+    start_tulip = "D:\Lab Python\Lab_1\\"
+    file = "D:\Lab Python\\"
      
-    write_csv(directory_rose, start_rose, "rose")
-    write_csv(directory_tulip, start_tulip, "tulip")  
+    write_csv(directory_rose, start_rose, file, "rose")
+    write_csv(directory_tulip, start_tulip, file, "tulip")  
 
 if __name__ == "__main__":
 	main()
